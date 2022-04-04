78def9328785792a044174821412a9d7f701db62
David Saharkhiz <davidsakh@gmail.com>
2022-04-03 21:25:32 -0400

    working on setup

--- windows-commands/powershell-virtualenv-startup.ps1
+++ windows-commands/powershell-virtualenv-startup.ps1
@@ -1,4 +1,10 @@
 # In a new powershell window run:
 cd ~\python3\Scripts\
 ./activate
+cd ~\source\django-geospacial\
 # this will be used for local python testing only, everything else in git bash
+# pycharm is also running powershell, so just use this
+
+# you can run all your env config here
+#(python3) PS C:\Users\David Saharkhiz\python3\Scripts> pip install Django
+#etc etc
